3240dca4eb50146daff6bf5e25d6377fdd96bc18
Santhosh-110 <santhosh9360445398@gmail.com>
2025-04-23 13:04:08 +0530

    Add files via upload

---
+++ if_statement.py
@@ -0,0 +1,88 @@
+n=int(input("enter your marks :"))
+if(n>85):
+    print("your grade is A")
+elif(n>60):
+    print("your grade is B")
+elif(n>40):
+    print( "your grade is c")
+else:
+    print("fail")  
+
+
+
+n=int(input("enter your age :"))
+if(n>18):
+    print("your eligible for vote")
+else:
+    print("your not eligible for vote")
+
+a=int(input("enter the value :"))
+b=int(input("enter the value :"))
+c=int(input("enter the value :"))
+if(a>b and a>c):
+    print("a is greater")
+elif(b>a and b>c):
+    print(" b is greater")
+else:
+    print("c is greater")
+
+
+word=("hello wold")
+if(" w is present in word") :
+    print("w is present")
+else:
+    print("w  is not present")   
+
+
+n=int(input("enter the value :"))
+if(n>85):
+    print("your grade is A")
+elif(n>60):
+    print("your grade is B")
+elif(n>40):
+    print("your grade is c")
+else:
+    print("hi")
+
+
+
+#Task 3
+#2
+a=int(input("Enter your age: "))
+n=18
+if(n<a):
+    print("you are eligible in vote")
+elif(n==a):
+    print("Yor are eligible")
+else:
+    print("you are not eligible")
+
+#4
+a=int(input("Enter your i will tell your grade:"))
+if(a>=85):
+    print("A Grade")
+elif(a>=60):
+    print("B Grade")
+elif(a>=40):
+    print("C Grade")
+elif(a>=35):
+    print("C Grade")
+else:
+    print("fail")
+
+# #2
+n=int(input("Enter the num:  "))
+if(n%2==0):
+    print("even number")
+else:
+    print("odd number")
+#1
+a=int(input("Enter the num 1: "))
+b=int(input("Enter the num 2: "))
+c=int(input("Enter the num 3: "))
+if(a>b and a>c):
+    print("a is largest num")
+elif(b>a and b>c):
+    print("b is lardest num")
+else:
+    print("c is largest")    
