e3c7b442b273c74c81346e2fbeb07d5fb72c3b44
Santhosh-110 <santhosh9360445398@gmail.com>
2025-06-26 10:21:52 +0530

    Add files via upload

---
+++ bank_account.py
@@ -0,0 +1,47 @@
+# class person:
+#     # name=input("Enter the name: ")
+#     # age=int(input("Enter the age: "))
+#     # address=input("Enter the address: ")
+#     def __init__(self):
+#         print(f"hello my Name is")
+#     def printing(self,name,age,address):
+#         print(f"name:{name} age: {age} address:{address}")
+# lecture = person()
+# lecture.printing("santhosh",23,"Thirumayam")
+
+# class bank_account:
+#     account_holder="Sivaguru"
+#     print("Account_holder: ",account_holder)
+#     def account(self,n):
+#         print(f"Your Bank Account: {n}")
+#     def deposit(self,m):
+#         print(f"Deposit amount: {m}")
+#     def withdraw(self,w):
+#         print(f"Withdraw: {w}")
+#     def balence(b):
+#     #      b=1000+m-w
+#     # deposit()    
+#     # withdraw()
+# print(f"Your Balence={b}")
+        
+# accoun = bank_account()
+# # print(accoun.account_holder())
+# accoun.account(1000)
+# accoun.deposit(m=int(input("Enter the deposit ammount: ")))
+# accoun.withdraw(w=int(input("Enter the withdraw amount: ")))
+# accoun.balence()
+
+
+# class bankaccount:
+#     def account(self,name):
+#         self.name=name
+        
+#     def deposit(amount):
+#         print("Dposit Amount: ")
+#     def withdraw(amount):
+#         print("Withdraw Amount: ") 
+        
+# e1=bankaccount()
+# e1.account("siva")
+
+
